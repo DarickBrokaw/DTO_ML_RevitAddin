b7e9a481691815cfb1a9060ea9c4c46d8b7a7759
Darick Brokaw <93608701+DarickBrokaw@users.noreply.github.com>
2023-04-28 12:47:44 -0400

    Added functionality to download files from GitHub. In the previous commit, the program only logged the latest GitHub release version. Fixed issues related to GitHubReleaseChecker integration in the OnShutdown method by correcting the GetLatestVersionAsync method to return a GitHubRelease object instead of a string. Updated the OnShutdown method accordingly. Currently, the log incorrectly writes the release version as an object reference string (e.g., 'GitHubConnect.GitHubReleaseChecker+GitHubRelease') which needs to be fixed for proper logging.

--- GitHubReleaseChecker.cs
+++ GitHubReleaseChecker.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
@@ -19,30 +22,70 @@ namespace GitHubConnect
         
         }
 
-        public async Task<string> GetLatestVersionAsync(string repositoryOwner, string repositoryName)
+        public async Task<GitHubRelease> GetLatestVersionAsync(string repositoryOwner, string repositoryName)
         {
 
             var response = await _httpClient.GetAsync($"https://api.github.com/repos/{repositoryOwner}/{repositoryName}/releases/latest");
 
             if (response.IsSuccessStatusCode)
             {
-                //var json = await response.Content.ReadAsStringAsync();
-                //var release = JsonSerializer.Deserialize<GitHubRelease>(json);
                 var json = await response.Content.ReadAsStreamAsync();
                 var serializer = new DataContractJsonSerializer(typeof(GitHubRelease));
                 var release = (GitHubRelease)serializer.ReadObject(json);
-                return release.TagName;
+                return release;
             }
             else
             {
                 throw new Exception($"Error fetching latest release for repository '{repositoryOwner}/{repositoryName}'");
             }
         }
+
+        public async Task DownloadReleaseAssetsAsync(GitHubRelease release, string destinationFolder, List<string> assetFileNamesToDownload = null)
+        {
+            if (!Directory.Exists(destinationFolder))
+            {
+                Directory.CreateDirectory(destinationFolder);
+            }
+
+            var assetsToDownload = assetFileNamesToDownload == null ? release.Assets : release.Assets.Where(asset => assetFileNamesToDownload.Contains(asset.Name)).ToList();
+
+            foreach (var asset in assetsToDownload)
+            {
+                string destinationFilePath = Path.Combine(destinationFolder, asset.Name);
+                await DownloadFileAsync(asset.DownloadUrl, destinationFilePath);
+            }
+        }
+
+        private async Task DownloadFileAsync(string downloadUrl, string destinationFilePath)
+        {
+            using (var response = await _httpClient.GetAsync(downloadUrl))
+            {
+                using (var fileStream = File.Create(destinationFilePath))
+                {
+                    await response.Content.CopyToAsync(fileStream);
+                }
+            }
+        }
+
+
         [DataContract]
-        private class GitHubRelease
+        public class GitHubRelease
         {
             [DataMember(Name = "tag_name")]
             public string TagName { get; set; }
+
+            [DataMember(Name = "assets")]
+            public List<GitHubAsset> Assets { get; set; }
+        }
+
+        [DataContract]
+        public class GitHubAsset
+        {
+            [DataMember(Name = "name")]
+            public string Name { get; set; }
+
+            [DataMember(Name = "browser_download_url")]
+            public string DownloadUrl { get; set; }
         }
     }
 }
---
+++ GitHubReleaseChecker_v1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GitHubConnect
+{
+    public class GitHubReleaseChecker
+    {
+        private readonly HttpClient _httpClient;
+
+        public GitHubReleaseChecker()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "AutoUpdater");
+            _httpClient.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
+        
+        }
+
+        public async Task<string> GetLatestVersionAsync(string repositoryOwner, string repositoryName)
+        {
+
+            var response = await _httpClient.GetAsync($"https://api.github.com/repos/{repositoryOwner}/{repositoryName}/releases/latest");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStreamAsync();
+                var serializer = new DataContractJsonSerializer(typeof(GitHubRelease));
+                var release = (GitHubRelease)serializer.ReadObject(json);
+                return release.TagName;
+            }
+            else
+            {
+                throw new Exception($"Error fetching latest release for repository '{repositoryOwner}/{repositoryName}'");
+            }
+        }
+        [DataContract]
+        private class GitHubRelease
+        {
+            [DataMember(Name = "tag_name")]
+            public string TagName { get; set; }
+        }
+    }
+}
